746d215354b0d1ee7a9cef28feeb854cdd8adf91
rhsadikbd <sadikcsbd@gmail.com>
2025-01-01 14:21:05 +0600

    initial commit

---
+++ src/components/JobListings.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import Joblisting from "./Joblisting.vue";
+import jobData from "@/jobs.json";
+import { ref } from "vue";
+
+const jobs = ref(jobData);
+
+defineProps({
+    limit: Number,
+    showButton: {
+        Type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<template>
+    <section class="bg-green-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <Joblisting
+                    v-for="job in jobs.slice(0, limit || jobs.length)"
+                    :key="job.id"
+                    :job="job"
+                />
+            </div>
+        </div>
+    </section>
+    <section class="m-auto max-w-lg my-10 px-6" v-if="showButton">
+        <RouterLink
+            to="/jobs"
+            class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+            >View All Jobs</RouterLink
+        >
+    </section>
+</template>
